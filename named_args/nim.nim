aada5ae8604d8fec279fc4189d974a2cb7e8e6a5
gavr <gavr123456789@gmail.com>
2021-05-24 01:21:40 +0300

    constructor named args example

--- named_args/nim.nim
+++ named_args/nim.nim
@@ -1,11 +1,16 @@
 type 
   Sas = ref object
+    age: int
+    name: string
   Sus = ref object
 
 proc newBuilderListItemFactoryFromBytes*(scope: Sas | Sus = nil;
     bytes: bool) =
   echo "sas"
   
-
+# proc named args with =
 newBuilderListItemFactoryFromBytes(scope= nil, bytes= true)
 newBuilderListItemFactoryFromBytes(bytes= true)
+
+# constructor named args with :
+var sas = Sas(age: 42, name: "sas")
