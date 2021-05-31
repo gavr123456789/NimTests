82a620235b8e87faee91af7ac3ad9cded243536c
g.maksutenko <g.maksutenko@indevstudio.com>
2021-05-31 15:53:51 +0300

    change jsonList to editable list

--- jsonAsList/nim.nim
+++ jsonAsList/nim.nim
@@ -2,43 +2,108 @@ import gintro/[gtk4, gobject, gio]
 import std/with
 import Types
 
+
+type
+  AddControlData = tuple 
+    entry: Entry
+    list: StringList
+  DeleteControlData = tuple
+    selection: SingleSelection
+    list: StringList
+
 const mockData = create10Sas().convertSeqSasToSting()
 
+
+### Utils
+func getNItems(self: StringList): int = 
+  let lm = cast[ListModel](self)
+  return lm.getNItems()
+
+proc getString(self: ListItem): string = 
+  let strobj = cast[StringObject](self.getItem())
+  result = gtk4.getString(strobj)
+
+func generate100Strings(itemNum: int): seq[string] = 
+  for index in itemNum..itemNum + 100:
+    result.add "item №" & $index
+
+### SignalFactory callbacks
 proc setup_cb(factory: gtk4.SignalListItemFactory, listitem: gtk4.ListItem) =
   listitem.setChild(newLabel(""))
   
 proc bind_cb(factory: gtk4.SignalListItemFactory, listitem: gtk4.ListItem) =
-  var 
+  let 
+    strFromItem = listitem.getString()
     lb = listitem.getChild().Label
-    strobj = cast[StringObject](listitem.getItem())
-    txtfromobj = gtk4.getString(strobj)
-  echo txtfromobj
-  let textSas = txtfromobj.sasFromString()
+    textSas = strFromItem.sasFromString()
   lb.text = textSas.name & ": " & $textSas.age
+  
+
+
+
 
 proc unbind_cb(factory: gtk4.SignalListItemFactory, listitem: gtk4.ListItem) =
-  # There's nothing to do here. 
-  # If you does something like setting a signal in bind_cb,
-  # then disconnecting the signal is necessary in unbind_cb. 
   echo "unbind"
 
 proc teardown_cb(factory: gtk4.SignalListItemFactory, listitem: gtk4.ListItem) =
   listitem.setChild (nil)
-  # When the child of listitem is set to NULL, the reference to GtkLabel will be released and lb will be destroyed. 
-  # Therefore, g_object_unref () for the GtkLabel object doesn't need in the user code. 
+
+
+### Controls callbacks
+func btnAddCb(btn: Button, controlData: AddControlData) =
+  if controlData.entry.text != "":
+    controlData.list.append controlData.entry.text
+    controlData.entry.text = ""
+  
+func btnRemoveCb(btn: Button, data: DeleteControlData) =
+  data.list.remove data.selection.getSelected()
+  
+func btnRemoveAllCb(btn: Button, data: StringList) =
+  data.splice(0, data.getNItems())
+
+func btnAdd100Cb(btn: Button, data: StringList) =
+  let maxItemPosition = data.getNItems()
+  data.splice(maxItemPosition, 0, generate100Strings(maxItemPosition))
 
 proc activate(app: gtk4.Application) =
   let
+    # main
     window = newApplicationWindow(app)
-    scr = newScrolledWindow()
-
+    scrolled = newScrolledWindow()
+    mainBox = newBox(Orientation.vertical, 0)
+    # ListView
     sl = gtk4.newStringList(mockData)
     ls = cast[ListModel](sl)
-    ns = gtk4.newNoSelection(ls)
+    ns = gtk4.newSingleSelection(ls)
     factory = gtk4.newSignalListItemFactory()
     lv = newListView(ns, factory)
+    # Controls
+    controlBox = newBox(Orientation.horizontal, 0)
+    add = newButton("Add")
+    remove = newButton("Remove")
+    removeAll = newButton("Remove All")
+    add100 = newButton("Add 100")
+    entry = newEntry()
+
+  # Connect controls
+  add.connect("clicked", btnAddCb, (entry, sl))
+  remove.connect("clicked", btnRemoveCb, (ns, sl))
+  removeAll.connect("clicked", btnRemoveAllCb, sl)
+  add100.connect("clicked", btnAdd100Cb, sl)
+
   
-  scr.setChild lv
+  with controlBox:
+    halign= Align.center
+    setCssClasses("linked")
+    append add
+    append remove
+    append removeAll
+    append add100
+    append entry
+  
+  with scrolled:
+    setChild lv
+    vexpand = true
 
   with factory:
     connect("setup", setup_cb)
@@ -46,10 +111,14 @@ proc activate(app: gtk4.Application) =
     connect("unbind", unbind_cb)
     connect("teardown", teardown_cb)
 
+  with mainBox:
+    append scrolled
+    append controlBox
+
   with window:
-    defaultSize = (600, 400)
-    title = "Nim ListView"
-    setChild scr
+    defaultSize = (500, 300)
+    title = "Nim Simple Todo Example"
+    setChild mainBox
     show
 
 proc main =
