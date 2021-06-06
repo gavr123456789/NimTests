643e89dc1dac72ae841f2102efb2baa74893aca0
gavr <gavr123456789@gmail.com>
2021-06-06 21:53:36 +0300

    simple sum type

---
+++ sum_types/intOrString.nim
@@ -0,0 +1,10 @@
+type
+  IntOrStringKind = enum Int, String
+  IntOrString = object
+    case kind: IntOrStringKind
+    of Int: intVal: int
+    of String: strVal: string
+
+var x: seq[IntOrString]
+x = @[IntOrString(kind: Int, intVal: 42), IntOrString(kind: String, strVal: "Hello world")]
+echo x # @[(kind: Int, intVal: 42), (kind: String, strVal: "Hello world")]
