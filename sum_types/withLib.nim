c4279857d3477c40c81d6e5d5ed06d531fd829d9
g.maksutenko <g.maksutenko@indevstudio.com>
2021-06-07 13:02:28 +0300

    add seq

--- sum_types/withLib.nim
+++ sum_types/withLib.nim
@@ -21,3 +21,24 @@ a.unpack(someName): # Unpacks it as `someName`
   echo someName
 
 a = "sas" # Implicit converter
+
+### Seq
+sumTypeSeq(IntOrStringSeq, AcceptedTypes)
+
+var yourSeq: IntOrStringSeq
+yourSeq.add 100
+yourSeq.add "sas"
+
+assert yourSeq.toSeq(string) == @["sas"] # `toSeq` will return a new seq of the type queried.
+yourSeq.drop(string) # This will remove all instances of the `float` variant from the list.
+yourSeq.filter(int) # This will remove all other types other than `int`
+assert yourSeq.len == 1
+
+yourSeq[0] = 100 # Assigns `0` to a new variant with value 100.
+
+yourSeq[0] = initIntOrStringSeqEntry(100) # The above is the same as doing this, makes new variant and assigns it.
+
+unpack(yourSeq.pop): # Removes the last element, passing `it` into the body.
+  echo it
+assert yourSeq.len == 0
+
